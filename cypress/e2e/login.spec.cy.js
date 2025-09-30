d4a472f08c508d9193e1ccacbb0e8baa4897ee28
Maria Eduarda <mariaeduarda.fdsouza@gmail.com>
2025-09-30 16:51:39 -0300

    User update scenario added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -13,6 +13,10 @@ const selectorsList = {
   FirstNameField: '[name="firstName"]',
   LastNameField: '[name="lastName"]',
   MidleNameField: '[name="middleName"]',
+  genericField:'.oxd-input--active',
+  dateField: '[placeholder="yyyy-dd-mm"]',
+  dateCloseButton: '.--close',
+  saveButton: '.orangehrm-left-space'
 
 }
   it.only('User Info Update - Success', () => {
@@ -27,6 +31,13 @@ const selectorsList = {
     cy.get(selectorsList.FirstNameField).type('Jungkook')
     cy.get(selectorsList.LastNameField).clear().type('Jeon')
     cy.get(selectorsList.MidleNameField).clear().type('Kim')
+    cy.get(selectorsList.genericField).eq(3).clear().type('test')
+    cy.get(selectorsList.genericField).eq(4).clear().type('test')
+    cy.get(selectorsList.genericField).eq(5).clear().type('test')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2025-05-13')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.saveButton).eq(0).click()
+    cy.get('body').should('contain','Successfully Updated')
    
   })
   it('Login - Fail', () => {
